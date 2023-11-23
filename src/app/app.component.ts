e3c7a16c4647d0301f3931406c89dc8acb724440
Robert Gleason <l2ob122@gmail.com>
2023-11-22 20:15:01 -0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Item } from './item';
+import { ItemComponent } from "./item/item.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, ItemComponent]
 })
 export class AppComponent {
   title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all'; // union type
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play ', done: false },
+    { description: 'code', done: true },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
